b96bed6f523eebea089a99046f2ab1bb48f77206
Danyydze <danilamarkov2803@icloud.com>
2025-04-09 17:05:19 +0300

    Initial Commit

---
+++ KS-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KS-Test
+//
+//  Created by Данил Марков on 09.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
